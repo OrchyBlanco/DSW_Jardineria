753da98b39ce59594989bd91fd949116bad130c7
Sergio Gonzalez Rodriguez <sergioglezrguez06@gmail.com>
2021-10-20 13:00:15 +0100

    DSW - Project Generation
    Generation of the project DSW-JARDINERIA

---
+++ Practica DSW/DSW-JARDINERIA/DSW-JARDINERIA/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using DSW_JARDINERIA.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DSW_JARDINERIA.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Practica DSW/DSW-JARDINERIA/DSW-JARDINERIA/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DSW_JARDINERIA.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
