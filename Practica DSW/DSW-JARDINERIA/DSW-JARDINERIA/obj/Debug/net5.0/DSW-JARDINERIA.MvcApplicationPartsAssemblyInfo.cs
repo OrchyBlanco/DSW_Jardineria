0aa618899655bd798397c3a58e9e9ca5079f0a0c
Jejormi <jesus.torres.blanco@outlook.es>
2021-10-31 14:01:24 +0000

    DSW - Jardenieria - Paginacion, busqueda y ordenacion PRODUCTOS
    Se agrega la pagiancion y busqueda por Nombre de Productos.
    Tambien se agrega la Ordenacion pero no llega a ordenar.

--- Practica DSW/DSW-JARDINERIA/DSW-JARDINERIA/obj/Debug/net5.0/DSW-JARDINERIA.MvcApplicationPartsAssemblyInfo.cs
+++ Practica DSW/DSW-JARDINERIA/DSW-JARDINERIA/obj/Debug/net5.0/DSW-JARDINERIA.MvcApplicationPartsAssemblyInfo.cs
@@ -13,6 +13,7 @@ using System.Reflection;
 
 [assembly: Microsoft.AspNetCore.Mvc.ApplicationParts.ApplicationPartAttribute("Microsoft.AspNetCore.Identity.UI")]
 [assembly: Microsoft.AspNetCore.Mvc.ApplicationParts.ApplicationPartAttribute("Microsoft.AspNetCore.Identity.UI.Views.V4")]
+[assembly: Microsoft.AspNetCore.Mvc.ApplicationParts.ApplicationPartAttribute("X.PagedList.Mvc.Core")]
 
 // Generado por la clase WriteCodeFragment de MSBuild.
 
