5b45f70f78d498a118d397736da9fd7377fdaa48
Sergio Gonzalez Rodriguez <43896975+SergioGonzalezRodriguez@users.noreply.github.com>
2021-11-01 22:46:54 +0000

    Attr Added

--- Practica DSW/DSW-JARDINERIA/DSW-JARDINERIA/Models/GamaProducto.cs
+++ Practica DSW/DSW-JARDINERIA/DSW-JARDINERIA/Models/GamaProducto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 #nullable disable
 
@@ -12,9 +13,16 @@ namespace DSW_JARDINERIA.Models
             Productos = new HashSet<Producto>();
         }
 
+        [Required]
+        [StringLength(30)]
         public string Gama { get; set; }
+        [Required]
+        [StringLength(120)]
         public string DescripcionTexto { get; set; }
+        [Required]
+        [StringLength(120)]
         public string DescripcionHtml { get; set; }
+        [Required]
         public string Imagen { get; set; }
 
         public virtual ICollection<Producto> Productos { get; set; }
--- Practica DSW/DSW-JARDINERIA/DSW-JARDINERIA/Models/Producto.cs
+++ Practica DSW/DSW-JARDINERIA/DSW-JARDINERIA/Models/Producto.cs
@@ -1,9 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using System.ComponentModel.DataAnnotations;
 
 #nullable disable
-
 namespace DSW_JARDINERIA.Models
 {
     public partial class Producto
@@ -12,15 +11,31 @@ namespace DSW_JARDINERIA.Models
         {
             DetallePedidos = new HashSet<DetallePedido>();
         }
-        
+        [Required]
         public string CodigoProducto { get; set; }
+        [Required]
+        [StringLength(20)]
         public string Nombre { get; set; }
+        [Required]
+        [StringLength(20)]
         public string Gama { get; set; }
+        [Required]
+        [Range(0, double.MaxValue)]
         public string Dimensiones { get; set; }
+        [Required]
+        [StringLength(30)]
         public string Proveedor { get; set; }
+        [Required]
+        [StringLength(1000)]
         public string Descripcion { get; set; }
+        [Required]
+        [Range(0, int.MaxValue)]
         public short CantidadEnStock { get; set; }
+        [Required]
+        [Range(0, double.MaxValue)]
         public decimal PrecioVenta { get; set; }
+        [Required]
+        [Range(0, double.MaxValue)]
         public decimal? PrecioProveedor { get; set; }
 
         public virtual GamaProducto GamaNavigation { get; set; }
